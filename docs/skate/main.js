cff0aabb538a443396998bd76dfa8bb052bd8362
d-ney <d.ney210@gmail.com>
2021-10-11 14:43:43 -0700

    added basic ramps and pits

--- docs/skate/main.js
+++ docs/skate/main.js
@@ -23,7 +23,17 @@ lrrrrl
  rLLr 
 lrrrrl
 llrrll
+`,
+
 `
+bbbbbb
+bbbbbb
+bbbbbb
+bbbbbb
+ bbbb
+  bb
+`,
+
 ];
 
 const G = {
@@ -31,6 +41,8 @@ const G = {
   HEIGHT: 100,
 
   JUMP_HEIGHT: 4,
+  SPEED: 1,
+  RAMP_COOLDOWN: 200
 };
 
 options = {
@@ -40,7 +52,7 @@ options = {
   viewSize: {x: G.WIDTH, y: G.HEIGHT}
 };
 
-/** @typedef {{pos: Vector}} Player */
+/** @typedef {{pos: Vector, cooldown: number, inAir: boolean}} Player */
 /** @type { Player } */
 let player;
 
@@ -48,14 +60,94 @@ let player;
 /** @type { PlayerSprite } */
 let playerSprite;
 
+/** @typedef {{pos: Vector}} Ramp*/
+/** @type {Ramp} */
+let Ramp;
+
+/**
+* @type  { Ramp [] }
+*/
+let Ramps;
+
+/** @typedef {{pos: Vector, size: number, cooldown: number}} Pit */
+/** @type { Pit } */
+let Pit;
+
 function update() {
   if (!ticks) {
     player = {
       pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5 + 20),
+      cooldown: G.RAMP_COOLDOWN,
+      inAir: false
     };
     playerSprite = {
       pos: vec(player.pos.x, player.pos.y + 6)
     }
+
+    Ramps = times(3, () => {
+      const posX = rnd(0, G.WIDTH);
+      const posY = 0;
+      return{
+        pos: vec(posX, posY)
+      }
+    });
+
+    Pit = {
+      pos: vec(0, 0),
+      size: rndi (5, 10),
+      cooldown: 200
+    }
+    
+  }
+
+  player.cooldown --;
+  Pit.cooldown --;
+
+  console.log(Pit.cooldown);
+  if(Ramps.length === 0)
+  {
+    if(player.cooldown <= 0){
+      for(var i = 0; i < rndi(3,7); i++)
+      {
+        Ramps.push({pos: vec(rnd(0, G.WIDTH), 0)});
+      }
+      player.cooldown = G.RAMP_COOLDOWN;
+    }
+  }
+
+  if(Pit.cooldown < 0)
+  {
+    color("black");
+    rect(Pit.pos, G.WIDTH, Pit.size);
+    Pit.pos.y += G.SPEED;
+
+    if(Pit.pos.y >= G.HEIGHT) {
+      Pit.pos = vec(0,0);
+      Pit.cooldown = rndi(100, 200);
+    }
+
+  }
+
+  Ramps.forEach(r => {
+    r.pos.y += G.SPEED;
+    char("c", r.pos);
+    // if(ramp_cooldown <= 0){
+    //   ramp_cooldown = rndi(G.RAMP_COOLDOWN / 2, G.RAMP_COOLDOWN * 2);
+    // }
+    // else
+    //   ramp_cooldown --;
+
+    remove(Ramps, (r) => {
+      return r.pos.y > G.HEIGHT;
+    });
+  });
+
+  if(input.isPressed) {
+    player.inAir = true;
+  }
+
+  if(input.isJustReleased) {
+    player.inAir = false;
   }
 
   // Drawing double sprites
@@ -66,5 +158,10 @@ function update() {
   color('black');
   char("a", player.pos);
   char("b", playerSprite.pos);
+
+  if(char("a", player.pos).isColliding.rect.black && !player.inAir)
+    end("Try again!");
+
+
 }
 
