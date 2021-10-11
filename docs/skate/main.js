487ba0a10cda4121b3d0e0a4772fca6a06e4c7af
jlao3 <j.chancelao@gmail.com>
2021-10-10 21:43:46 -0700

    Added barebones, skateboard character etc

--- docs/skate/main.js
+++ docs/skate/main.js
@@ -1,13 +1,45 @@
-title = "";
+title = `Skate 4`;
 
 description = `
 `;
 
-characters = [];
+characters = [
+// SKATEBOARD
+`
+llrrll
+lrrrrl
+ rrrr 
+ rrrr 
+lrrrrl
+llrrll
+`
+];
 
-options = {};
+const G = {
+  WIDTH: 100,
+  HEIGHT: 100,
+
+  JUMP_HEIGHT: 4,
+};
+
+options = {
+  theme: 'simple',
+  isPlayingBgm: true,
+  seed:1124, //4,7,9,11,65, 1124
+  viewSize: {x: G.WIDTH, y: G.HEIGHT}
+};
+
+/** @typedef {{pos: Vector}} Player */
+/** @type { Player } */
+let player;
 
 function update() {
   if (!ticks) {
+    player = {
+      pos: vec(G.WIDTH / 2, G.HEIGHT / 2),
+    };
   }
+
+  color('black');
+  char("a", player.pos)
 }
